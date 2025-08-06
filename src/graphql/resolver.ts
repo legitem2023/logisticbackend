6a61ede2e850459305188476f984ddf81b7e4551
Robert Marquez <legitem2023@gmail.com>
2025-08-06 08:55:46 +0800

    Update resolver.ts

--- src/graphql/resolver.ts
+++ src/graphql/resolver.ts
@@ -824,7 +824,39 @@ if (!user) {
       } catch (error) {
        console.log(error); 
       }
-    }
+    },
+    uploadFile: async (_parent, { file }) => {
+      // Wait for the file promise to resolve
+      const { createReadStream, filename, mimetype, encoding } = await file;
+
+      // Choose where to save the file
+      const uploadDir = path.join(process.cwd(), 'public/uploads');
+
+      // Make sure uploads folder exists
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+      }
+
+      // Create file path
+      const filePath = path.join(uploadDir, filename);
+
+      // Save file to disk
+      return new Promise((resolve, reject) => {
+        const stream = createReadStream();
+        const out = fs.createWriteStream(filePath);
+        stream.pipe(out);
+        out.on('finish', () =>
+          resolve({
+            filename,
+            mimetype,
+            encoding,
+            url: `/public/uploads/${filename}`, // you can serve this statically
+          })
+        );
+        out.on('error', reject);
+      });
+    },
+ 
  },
 Subscription: {
   LocationTracking: {
